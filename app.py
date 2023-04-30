c17860ec1a5efe2d9690fb427852cd6f25656a0f
bismayan <bismayan@gmail.com>
2023-04-29 22:03:21 -0400

    Added wikipedia lookup

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
-
+from langchain.utilities import WikipediaAPIWrapper
 
 def set_openai_key(ret_value: bool = False):
     """Reads the Open AI API Key and sets the env variable OPENAI_API_KEY with the value.
@@ -37,12 +37,13 @@ def initialize_templates():
     """
     title_template = PromptTemplate(input_variables=['topic'],
                                     template="Write me a Youtube video title about {topic}")
-    script_template = PromptTemplate(input_variables=['title'],
-                                     template="Write me a short 300 word Youtube Video script based on the title TITLE: {title}")
+    script_template = PromptTemplate(input_variables=['title', 'wikipedia_research'],
+                                     template="Write me a short 300 word Youtube Video script based on the title TITLE: {title}, \
+                                        while also using this wikipedia research: {wikipedia_research}")
     return script_template, title_template
 
 
-def initialize_chain(title_template, script_template, memory):
+def initialize_chain(title_template, script_template, t_memory, s_memory):
     """Returns the final Langchain Sequential Chain
 
     Args:
@@ -53,29 +54,37 @@ def initialize_chain(title_template, script_template, memory):
         SimpleSequentialChain: Returns the FInal LLM Chain with the Two LLMs chained
     """
     llm = OpenAI(temperature=0.9)
-    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key="title", memory=memory)
-    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key="script", memory=memory)
-    final_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], 
-                                  output_variables=['title', 'script'], verbose=True)
-    return final_chain
+    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key="title", memory=t_memory)
+    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key="script", memory=s_memory)
+    return title_chain, script_chain
 
 
 def main():
     script_template, title_template = initialize_templates()
-    memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
-    final_chain = initialize_chain(title_template, script_template, memory)
-    
+    title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+    script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+    title_chain, script_chain = initialize_chain(title_template, script_template, title_memory, script_memory)
+    wiki= WikipediaAPIWrapper()
+
+
     st.title("🤖 🤖 Welcome to Bismayan++!")
     pr = st.text_input(
         "Unlike my creator, I am somewhat intelligent. What would you like to get the script for?", value="")
     if pr:
         with st.spinner(text="Working on it..."):
-            response = final_chain({"topic":pr})
-            st.write(response['title'])
-            st.write(response['script'])
+            title = title_chain.run(pr)
+            wiki_research= wiki.run(pr)
+            script= script_chain.run(title=title, wikipedia_research= wiki_research)
+            st.write(title)
+            st.write(script)
 
-            with st.expander("Message History"):
-                st.info(memory.buffer)
+            with st.expander("Title History"):
+                st.info(title_memory.buffer)
+            with st.expander("Script History"):
+                st.info(script_memory.buffer)
+            with st.expander("Wikipedia Research"):
+                st.info(wiki_research)
+
 
 
 if __name__ == "__main__":
