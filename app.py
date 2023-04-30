40a21ba42ae4c1063b6aa28902b07b188acaafeb
bismayan <bismayan@gmail.com>
2023-04-29 21:37:05 -0400

    Added Memory, message history and loading spinner

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import os
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
 
 
 def set_openai_key(ret_value: bool = False):
@@ -37,11 +38,11 @@ def initialize_templates():
     title_template = PromptTemplate(input_variables=['topic'],
                                     template="Write me a Youtube video title about {topic}")
     script_template = PromptTemplate(input_variables=['title'],
-                                     template="Write me a Youtube Video script based on the title TITLE: {title}")
+                                     template="Write me a short 300 word Youtube Video script based on the title TITLE: {title}")
     return script_template, title_template
 
 
-def initialize_chain(title_template, script_template):
+def initialize_chain(title_template, script_template, memory):
     """Returns the final Langchain Sequential Chain
 
     Args:
@@ -52,22 +53,29 @@ def initialize_chain(title_template, script_template):
         SimpleSequentialChain: Returns the FInal LLM Chain with the Two LLMs chained
     """
     llm = OpenAI(temperature=0.9)
-    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-    final_chain = SimpleSequentialChain(
-        chains=[title_chain, script_chain], verbose=True)
+    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key="title", memory=memory)
+    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key="script", memory=memory)
+    final_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], 
+                                  output_variables=['title', 'script'], verbose=True)
     return final_chain
 
 
 def main():
     script_template, title_template = initialize_templates()
-    final_chain = initialize_chain(title_template, script_template)
+    memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+    final_chain = initialize_chain(title_template, script_template, memory)
+    
     st.title("🤖 🤖 Welcome to Bismayan++!")
     pr = st.text_input(
         "Unlike my creator, I am somewhat intelligent. What would you like to get the script for?", value="")
     if pr:
-        response = final_chain.run(pr)
-        st.write(response)
+        with st.spinner(text="Working on it..."):
+            response = final_chain({"topic":pr})
+            st.write(response['title'])
+            st.write(response['script'])
+
+            with st.expander("Message History"):
+                st.info(memory.buffer)
 
 
 if __name__ == "__main__":
